a28c76404258fc0d225bf3b27a24f53cc1a59afa
Nitesh Seram <niteshseram@gmail.com>
2024-04-28 14:30:26 +0530

    newsletter section challenge

---
+++ newsletter-section/js/index.js
@@ -0,0 +1,47 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const emailField = document.getElementById("email");
+const emailError = document.getElementById("email-error");
+
+const toast = document.getElementById("toaster");
+
+const form = document.getElementById("form");
+
+emailField.addEventListener("input", function () {
+  if (this.validity.valid) {
+    emailError.style.display = "none";
+  } else if (this.validity.typeMismatch) {
+    emailError.style.display = "block";
+    emailError.textContent = "Please enter valid email address.";
+  } else if (this.validity.valueMissing) {
+    emailError.style.display = "block";
+    emailError.textContent = "Email address is required";
+  }
+});
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  //clear all the values after submit
+  emailField.value = "";
+  toast.style.display = "flex";
+
+  const toastContent = toast.firstElementChild;
+  const toastContentBadge = toastContent.firstElementChild;
+  const toastContentMessage = toastContent.lastElementChild;
+
+  const success = true;
+
+  if (success) {
+    toastContent.classList.add("toast__success");
+    toastContentBadge.innerText = "Success";
+    toastContentMessage.innerText =
+      "Subscription successful! Please check your email to confirm.";
+  } else {
+    toastContent.classList.add("toast__error__content");
+    toastContentBadge.classList.add("toast__error__badge");
+    toastContentBadge.innerText = "Error";
+    toastContentMessage.innerText =
+      "Failed to subscribe. Please ensure your email is correct or try again later.";
+  }
+}
