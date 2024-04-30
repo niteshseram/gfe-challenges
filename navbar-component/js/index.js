503156bed66c1e762717bbc8553689e277e4f5b4
Nitesh Seram <niteshseram@gmail.com>
2024-05-01 00:55:14 +0530

    Navbar component challenge

---
+++ navbar-component/js/index.js
@@ -0,0 +1,11 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const sideNavbar = document.getElementById("sideNavbar");
+
+function onOpenNavbar() {
+  sideNavbar.style.left = "0";
+}
+
+function onCloseNavbar() {
+  sideNavbar.style.left = "-100%";
+}
