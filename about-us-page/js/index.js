6fafca040160db496f11d7899871a7c50291390b
Nitesh Seram <niteshseram@gmail.com>
2024-05-06 18:18:27 +0530

    Remove unnecessary js from about us challenge

--- about-us-page/js/index.js
+++ about-us-page/js/index.js
@@ -11,26 +11,6 @@ function onCloseNavbar() {
   sideNavbar.style.left = "-100%";
 }
 
-// FAQ
-const accordions = document.getElementsByClassName("faq__accordion__label");
-
-for (let i = 0; i < accordions.length; i++) {
-  accordions[i].addEventListener("click", function () {
-    const faqContent = this.nextElementSibling;
-    const activeIcon = this.lastElementChild.firstElementChild;
-    const inactiveIcon = this.lastElementChild.lastElementChild;
-    if (faqContent.style.display === "none") {
-      faqContent.style.display = "block";
-      activeIcon.style.display = "block";
-      inactiveIcon.style.display = "none";
-    } else {
-      faqContent.style.display = "none";
-      activeIcon.style.display = "none";
-      inactiveIcon.style.display = "block";
-    }
-  });
-}
-
 // Contact
 const contactNameField = document.getElementById("contactName");
 const contactNameError = document.getElementById("contactNameError");
