35ba2a9131cf701221011a316061c89b00cad9fb
Nitesh Seram <niteshseram@gmail.com>
2024-05-21 13:47:23 +0530

    Profile card challenge with react

--- profile-card-react/src/App.js
+++ profile-card-react/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import clsx from 'clsx';
+import ProfileCard from './components/ProfileCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={clsx(
+        'min-h-screen bg-gradient',
+        'flex justify-center items-start',
+        'px-[17.5px] pt-[200px]'
+      )}>
+      <ProfileCard />
     </div>
   );
 }
---
+++ profile-card-react/src/components/ProfileCard.js
@@ -0,0 +1,89 @@
+import clsx from 'clsx';
+import React from 'react';
+import {
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiInstagramFill,
+  RiTwitterXFill,
+} from '@remixicon/react';
+
+const socials = [
+  {
+    icon: RiGithubFill,
+    url: 'https://github.com',
+    name: 'Link to Github profile',
+  },
+  {
+    icon: RiLinkedinBoxFill,
+    url: 'https://linkedin.com',
+    name: 'Link to Linkedin profile',
+  },
+  {
+    icon: RiInstagramFill,
+    url: 'https://instagram.com',
+    name: 'Link to Instagram profile',
+  },
+  {
+    icon: RiTwitterXFill,
+    url: 'https://twitter.com',
+    name: 'Link to Twitter profile',
+  },
+];
+
+const ProfileCard = () => {
+  return (
+    <div
+      className={clsx(
+        'bg-white w-[340px] px-[15px] py-[23px]',
+        'flex flex-col items-center gap-10',
+        'border border-neutral-200 rounded-lg'
+      )}>
+      <div className="flex flex-col items-center gap-6">
+        <img src="./profile.png" alt="Sarah Dole's profile" className="w-16" />
+        <div
+          className={clsx('flex flex-col items-center gap-[3px]', 'mt-[1px]')}>
+          <div className="font-medium text-xl text-neutral-900">Sarah Dole</div>
+          <div className="text-sm text-neutral-600 text-center">
+            Front End Engineer @ Microsoft
+          </div>
+        </div>
+        <p class="text-center text-neutral-600">
+          I turn coffee into bugs which are fixed by someone else. Certified
+          Stack Overflow and ChatGPT developer.
+        </p>
+      </div>
+      <div className="flex flex-col gap-6 w-full items-center">
+        <button
+          className={clsx(
+            'rounded shadow bg-indigo-700',
+            'text-white font-medium cursor-pointer',
+            'h-11 px-3.5 w-full',
+            'hover:bg-indigo-800 focus:bg-indigo-800 focus:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12]',
+            'disabled:pointer-events-none disabled:shadow-none disabled:bg-neutral-100 disabled:text-neutral-400'
+          )}>
+          Contact me
+        </button>
+
+        <div className="flex space-x-4">
+          {socials.map(({ icon: Icon, url, name }) => (
+            <a
+              key={url}
+              href={url}
+              aria-label={name}
+              target="_blank"
+              rel="noreferrer"
+              tabIndex={0}
+              className={clsx(
+                'cursor-pointer p-2 rounded',
+                'hover:bg-neutral-50 focus:bg-neutral-50 focus:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12]'
+              )}>
+              <Icon className="fill-indigo-700" size={20} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
