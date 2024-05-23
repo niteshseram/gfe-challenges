f09632f1bba59e19fe2328b72b4b057fd1796fee
Nitesh Seram <niteshseram@gmail.com>
2024-05-23 22:09:00 +0530

    Toggle aria-expanded value for accordion component

--- pricing-page/js/index.js
+++ pricing-page/js/index.js
@@ -58,6 +58,14 @@ const accordions = document.getElementsByClassName("faq__accordion__label");
 
 for (let i = 0; i < accordions.length; i++) {
   accordions[i].addEventListener("click", function () {
+    let ariaExpandedValue = accordions[i].getAttribute('aria-expanded');
+    if (ariaExpandedValue == 'true') {
+      ariaExpandedValue = 'false';
+    } else {
+      ariaExpandedValue = 'true';
+    }
+    accordions[i].setAttribute('aria-expanded', ariaExpandedValue);
+    
     const faqContent = this.nextElementSibling;
     const activeIcon = this.lastElementChild.firstElementChild;
     const inactiveIcon = this.lastElementChild.lastElementChild;
