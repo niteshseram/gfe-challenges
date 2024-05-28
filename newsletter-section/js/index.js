ceeccaff40271e6fb404c79f0d173ee618e6fbff
Nitesh Seram <niteshseram@gmail.com>
2024-05-28 15:47:33 +0530

    Update newsletter section challenge with grid and APi fetch

--- newsletter-section/js/index.js
+++ newsletter-section/js/index.js
@@ -1,50 +1,61 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
 const emailField = document.getElementById("email");
-const emailError = document.getElementById("emailError");
+const emailError = document.getElementById('email-error');
 
-const toast = document.getElementById("toaster");
+const toast = document.getElementById('toaster');
 
-const form = document.getElementById("form");
+const form = document.getElementById('form');
 
-emailField.addEventListener("input", function () {
+emailField.addEventListener('input', function () {
   if (this.validity.valid) {
-    emailError.style.display = "none";
-    emailField.classList.remove("input__field--error");
+    emailError.style.display = 'none';
+    emailField.classList.remove('input__field--error');
   } else if (this.validity.typeMismatch) {
-    emailError.style.display = "block";
-    emailError.textContent = "Please enter valid email address.";
-    emailField.classList.add("input__field--error");
+    emailError.style.display = 'block';
+    emailError.textContent = 'Please enter valid email address.';
+    emailField.classList.add('input__field--error');
   } else if (this.validity.valueMissing) {
-    emailError.style.display = "block";
-    emailError.textContent = "Email address is required";
-    emailField.classList.add("input__field--error");
+    emailError.style.display = 'block';
+    emailError.textContent = 'Email address is required';
+    emailField.classList.add('input__field--error');
   }
 });
 
-function onSubmit(event) {
+async function onSubmit(event) {
   event.preventDefault();
 
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email: emailField.value,
+    }), // Send the data in JSON format
+  };
+
+  // Make the request
+  const response = await fetch(
+    'https://www.greatfrontend.com/api/projects/challenges/newsletter',
+    requestOptions
+  );
+  const result = await response.json();
+
   //clear all the values after submit
-  emailField.value = "";
-  toast.style.display = "flex";
+  emailField.value = '';
+  toast.style.display = 'flex';
 
   const toastContent = toast.firstElementChild;
   const toastContentBadge = toastContent.firstElementChild;
   const toastContentMessage = toastContent.lastElementChild;
 
-  const success = true;
-
-  if (success) {
-    toastContent.classList.add("toast__content--success");
-    toastContentBadge.innerText = "Success";
-    toastContentMessage.innerText =
-      "Subscription successful! Please check your email to confirm.";
+  if (response.ok) {
+    toastContent.classList.add('toast__content--success');
+    toastContentBadge.innerText = 'Success';
+    toastContentMessage.innerText = result.message;
   } else {
-    toastContent.classList.add("toast__content--error");
-    toastContentBadge.classList.add("toast__content__badge--error");
-    toastContentBadge.innerText = "Error";
-    toastContentMessage.innerText =
-      "Failed to subscribe. Please ensure your email is correct or try again later.";
+    toastContent.classList.add('toast__content--error');
+    toastContentBadge.classList.add('toast__content__badge--error');
+    toastContentBadge.innerText = 'Error';
+    toastContentMessage.innerText = result.error;
   }
 }
