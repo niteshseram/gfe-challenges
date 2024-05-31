0244f54588f4efd63ab6989ab0dc1e3378cee163
Nitesh Seram <niteshseram@gmail.com>
2024-05-31 16:57:51 +0530

    Product specifications challenge

--- product-specification-section/src/App.js
+++ product-specification-section/src/App.js
@@ -1,5 +1,18 @@
+import clsx from 'clsx';
+import ProductSpecificationSection from 'src/components/ProductSpecificationSection';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <main className="min-h-screen p-4 max-w-[1440px] mx-auto">
+      <div
+        className={clsx(
+          'rounded-md bg-white min-h-[calc(100vh_-_32px)]',
+          'shadow-sm md:shadow-md lg:shadow-lg'
+        )}>
+        <ProductSpecificationSection />
+      </div>
+    </main>
+  );
 }
 
 export default App;
