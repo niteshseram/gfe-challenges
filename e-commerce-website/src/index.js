bd4bf24d5053fc77e45ab7c33885b1117e18fd69
Nitesh Seram <niteshseram@gmail.com>
2024-06-17 21:42:33 +0530

    Add scrollToTop on page change

--- e-commerce-website/src/index.js
+++ e-commerce-website/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
