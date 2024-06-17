52f23934cf240c22b0df223185e3a9cb1f1d8721
Nitesh Seram <niteshseram@gmail.com>
2024-06-17 15:11:31 +0530

    Order success section

---
+++ order-success-section/src/App.js
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+
+import Layout from './Layout';
+import CartPage from './pages/Cart';
+import CheckoutPage from './pages/Checkout';
+import OrderSuccessPage from './pages/OrderSuccess';
+
+import CartContextProvider from './context/CartContext';
+import ToastContextProvider from './context/ToastContext';
+
+function App() {
+  return (
+    <ToastContextProvider>
+      <CartContextProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/order" element={<OrderSuccessPage />} />
+          </Route>
+        </Routes>
+      </CartContextProvider>
+    </ToastContextProvider>
+  );
+}
+
+export default App;
